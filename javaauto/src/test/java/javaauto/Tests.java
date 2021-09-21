0ae906be50ef1fab2e1243236b4d07704aebadf3
tensingn <tensingn@oregonstate.edu>
2021-09-21 08:01:42 -0400

    restructuring files

--- javaauto/src/test/java/javaauto/Tests.java
+++ javaauto/src/test/java/javaauto/Tests.java
@@ -1,8 +1,12 @@
 package javaauto;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Tests {
+	public static void main(String[] args) {
+		test();
+	}
 	
 	@Test
 	public static void test() {
