d8f0b997b163ce66dadd60865d0a1a9c1146aa2a
tensingn <tensingn@oregonstate.edu>
2021-09-21 07:43:04 -0400

    restructuring files

---
+++ javaauto/src/test/java/javaauto/Tests.java
@@ -0,0 +1,11 @@
+package javaauto;
+
+import org.testng.annotations.Test;
+
+public class Tests {
+	
+	@Test
+	public static void test() {
+		System.out.println("woo");
+	}
+}
---
+++ javaauto/src/test/java/utilities/Utilities.java
@@ -0,0 +1,25 @@
+package utilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Utilities {
+	public static ChromeDriver startDriver(HashMap<String, String> config) {
+		System.setProperty("webdriver.chrome.driver", "/Applications/chromedriver");
+		
+		// check for null config
+		if(config == null) {
+			return new ChromeDriver();
+		}
+		
+		// set config properties
+		for(Map.Entry<String, String> entry : config.entrySet()) {
+			// TODO - set config properties
+			System.out.println(entry.toString());
+		}
+
+		return new ChromeDriver();
+	}
+}
