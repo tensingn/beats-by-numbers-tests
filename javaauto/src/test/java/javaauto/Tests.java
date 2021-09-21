b254125c094a8590a0725257fbf963c834f5fded
Nicholas Tensing <50158491+tensingn@users.noreply.github.com>
2021-09-21 09:52:10 -0400

    Update Tests.java

--- javaauto/src/test/java/javaauto/Tests.java
+++ javaauto/src/test/java/javaauto/Tests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Tests {
+	// TODO - help > install new software > https://dl.bintray.com/testng-team/testng-eclipse-release
 	public static void main(String[] args) {
 		test();
 	}
